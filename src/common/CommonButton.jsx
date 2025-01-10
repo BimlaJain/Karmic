c3147c22e2ba76a86035972e19cca90dfef4c9fe
Vimal  Jain <vimaljain@Vimals-MacBook-Air.local>
2025-01-10 09:24:18 +0530

    add meta tag

--- src/common/CommonButton.jsx
+++ src/common/CommonButton.jsx
@@ -8,7 +8,7 @@ const CommonButton = (props) => {
             className={`bg-white text-black lg:py-3 py-2 xl:px-8 lg:px-7 px-5 rounded-[82px] font-bold lg:text-base text-sm leading-[20px] ${props.className}`}
         >
             {props.text.classStyle}
-            <span className={`${props.customClass} hidden`}>
+            <span className={`${props.customClass} `}>
                 <ButtonArrow />
             </span>
         </button>
